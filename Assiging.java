596f7bddf86729e4a7dfc4fd8e7a885588375b9d
shaiksha <shaiknaveed5811@gmail.com>
2024-08-21 22:02:10 +0530

    Day1 Working_with_DTypes

---
+++ Assiging.java
@@ -0,0 +1,26 @@
+package com.JavaCodingFromScratch;
+
+public class Assiging {
+
+	public static void main(String[] args) {
+		 short s=789;
+         int a=10;
+         char b ='a';
+         float f=2.55f;
+         double d=45.3d;
+         boolean bool=false;
+         byte by=44;
+         long r=9966307254L;
+         System.out.println(s);
+         System.out.println(a);
+         System.out.println(b);
+         System.out.println(f);
+         System.out.println(d);
+         System.out.println(bool);
+         System.out.println(by);
+         System.out.println(r);
+
+
+	}
+
+}
---
+++ DT_range.java
@@ -0,0 +1,18 @@
+package com.JavaCodingFromScratch;
+
+public class DT_range {
+
+	public static void main(String[] args) {
+//      1 byte =4bits
+//   size of data types
+      System.out.println("Byte :"+Byte.SIZE/8);
+      System.out.println("Short :"+Short.SIZE/8);
+      System.out.println("Character :"+Character.SIZE/8);
+      System.out.println("Boolean : "+Boolean.FALSE);
+      System.out.println("Integer :"+Integer.SIZE/8);
+      System.out.println("Float :"+Float.SIZE/8);
+      System.out.println("Long :"+Long.SIZE/8);
+      System.out.println("Double :"+Double.SIZE/8);
+	}
+
+}
