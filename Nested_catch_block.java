ba0fcd80cf20c57f84a2d449e70ed80b023f1a1a
shaiksha <shaiknaveed5811@gmail.com>
2024-08-22 16:26:03 +0530

    Day3 Working_with_Exceptions

---
+++ Nested_catch_block.java
@@ -0,0 +1,20 @@
+package com.JavaCodingFromScratch;
+
+public class Nested_catch_block {
+
+	public static void main(String[] args) {
+                  try {
+                	  int[] arr=new int[8];
+                	  arr[1]=45/0;
+                	  arr[10]=78;
+                  }
+                  catch(ArithmeticException ae){
+                	  System.out.println(ae.getMessage());
+                  }
+                  catch(ArrayIndexOutOfBoundsException a) {
+                	  System.out.println(a.getMessage());
+
+                                    }
+                                 
+                  }
+}
